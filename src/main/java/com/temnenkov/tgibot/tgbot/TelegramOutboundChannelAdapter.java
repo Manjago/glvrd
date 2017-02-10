e4850a32487cd173831d8f325ef10502e3d91b32
Kirill Temnenkov <kirill@temnenkov.com>
2017-02-10 09:42:10 +0300

    выдаем сообщения об ошибке юзверю, если не удалось отослать сообщение в телеграм

--- src/main/java/com/temnenkov/tgibot/tgbot/TelegramOutboundChannelAdapter.java
+++ src/main/java/com/temnenkov/tgibot/tgbot/TelegramOutboundChannelAdapter.java
@@ -2,6 +2,7 @@ package com.temnenkov.tgibot.tgbot;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.temnenkov.tgibot.http.TelegramHttpSender;
 import com.temnenkov.tgibot.tgapi.method.SendMessage;
 import com.temnenkov.tgibot.tgapi.method.TelegramMethod;
@@ -13,15 +14,21 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 
 public class TelegramOutboundChannelAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(TelegramOutboundChannelAdapter.class);
     private final Gson gson = new GsonBuilder().create();
     private TelegramHttpSender httpSender;
+
+    private MessageChannel outMessages;
 
     public void sendMessage(Message<SendMessage> msg) throws IOException {
       LOGGER.debug("got msg for sending {}", msg);
@@ -41,13 +48,35 @@ public class TelegramOutboundChannelAdapter {
             BufferedHttpEntity buf = new BufferedHttpEntity(ht);
             String responseContent = EntityUtils.toString(buf, StandardCharsets.UTF_8);
             LOGGER.debug("[InC] {}", responseContent);
+
+            JsonObject jsonObject = gson.fromJson(responseContent, JsonObject.class);
+            if (!jsonObject.get("ok").getAsBoolean()) {
+                LOGGER.error("Error sending request = {}, response = {}, responseContent", request, response, responseContent);
+                sendErrorMessage(message, responseContent);
+            }
+
+
         }
 
+    }
+
+    private void sendErrorMessage(SendMessage message, String errResponse) {
+
+        message.setParseMode(null);
+        message.setText(MessageFormat.format("Ошибка, сообщите разработчику: {0}", errResponse));
+
+        MessagingTemplate template = new MessagingTemplate();
+        template.send(outMessages, MessageBuilder.withPayload(message).build());
     }
 
     @Required
     public void setHttpSender(TelegramHttpSender httpSender) {
         this.httpSender = httpSender;
+    }
+
+    @Required
+    public void setOutMessages(MessageChannel outMessages) {
+        this.outMessages = outMessages;
     }
 
 
